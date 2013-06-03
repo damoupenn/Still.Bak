c60673e41ee56fe8bb4cf6cc7049660f6093d671
David F. Moore <damo@sas.upenn.edu>
2013-06-03 10:16:11 -0400

    Updated what goes into the ETC files. Garbage removal.

--- QManager/backup_scripts.py
+++ QManager/backup_scripts.py
@@ -4,13 +4,24 @@ import os
 from datetime import date
 
 EndDir = '/home/obs/Still.Bak/'
-Backups = [
+BackupDirs = [
     '/home/obs/Share/scripts/*',
     '/home/obs/QManager',
     '/home/obs/SetupUtils']
 
-for B in Backups:
+for B in BackupDirs:
     command = 'cp -r %s %s'%(B,EndDir)
+    os.system(command)
+
+ETCFiles = [
+    '/etc/network/interfaces',
+    '/etc/hosts',
+    '/etc/fstab',]
+
+if not os.path.exists(EndDir+'ETC'):
+    os.system('mkdir %sETC'%EndDir)
+for f in ETCFiles:
+    command = 'cp %s %sETC/'%(f.split('/')[-1] + '.bak',EndDir)
     os.system(command)
 
 os.system('rsync -zru /home/obs/Still.Bak damo@agena.physics.upenn.edu:~/')
