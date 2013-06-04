3db566de4e49f482a3c31e93628ad45aec06dcf5
obs <obs@QMaster.(none)>
2013-06-04 10:52:45 -0400

    Added new ETC files and updated backup_scripts.

---
+++ QManager/backup_scripts.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+import os
+from datetime import date
+
+EndDir = '/home/obs/Still.Bak/'
+BackupDirs = [
+    '/home/obs/Share/scripts/*',
+    '/home/obs/QManager',
+    '/home/obs/SetupUtils']
+
+for B in BackupDirs:
+    command = 'cp -r %s %s'%(B,EndDir)
+    os.system(command)
+
+ETCFiles = [
+    '/etc/network/interfaces',
+    '/etc/hosts',
+    '/etc/fstab',]
+
+if not os.path.exists(EndDir+'ETC'):
+    os.system('mkdir %sETC'%EndDir)
+for f in ETCFiles:
+    command = 'cp %s %sETC/%s'%(f,EndDir,f.split('/')[-1] + '.bak')
+    os.system(command)
+
+os.system('rsync -zru /home/obs/Still.Bak damo@agena.physics.upenn.edu:~/')
